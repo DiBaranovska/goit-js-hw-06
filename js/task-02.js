b13784f5d9f5c11ba4aecb7a4f175c41922ebe62
Diana Baranovska <dmitrudi@gmail.com>
2023-02-24 11:42:26 +0200

    вирішує другу задачу

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+let listEl = document.querySelector("#ingredients");
+
+const textItem = ingredients.map((element) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = element;
+  itemEl.classList.add("item");
+  return itemEl;
+});
+listEl.append(...textItem);

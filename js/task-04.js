fa079467de89874bcbd0d9f22c8705e7064a7308
Diana Baranovska <dmitrudi@gmail.com>
2023-02-25 22:04:14 +0200

    вирішує четверту задачу

---
+++ js/task-04.js
@@ -0,0 +1,20 @@
+const buttonList = document.querySelector('#counter');
+
+const buttonDecrementEl = buttonList.firstElementChild;
+const buttoIncrementEl = buttonList.lastElementChild;
+const resultEl = buttonDecrementEl.nextElementSibling;
+let counterValue = 0;
+
+buttonDecrementEl.addEventListener('click', () => {
+  counterValue -= 1;
+  resultEl.textContent = counterValue;
+  return;
+});
+
+buttoIncrementEl.addEventListener('click', () => {
+  counterValue += 1;
+  resultEl.textContent = counterValue;
+  return;
+});
+
+console.log(counterValue);

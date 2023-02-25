72ec2db2ab67e2b37a063fa015a001c14b12cffa
Diana Baranovska <dmitrudi@gmail.com>
2023-02-25 23:34:05 +0200

    вирішує 6ту задачу

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+//debugger;
+
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', () => {
+  console.log(inputEl.value.length);
+  if (inputEl.value.length === Number(inputEl.getAttribute('data-length'))) {
+    inputEl.classList.remove('invalid');
+    inputEl.classList.add('valid');
+  } else {
+    inputEl.classList.remove('valid');
+    inputEl.classList.add('invalid');
+  }
+});

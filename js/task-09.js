202b29e0b8ee081f83c02869ec019c8bf4f53750
Diana Baranovska <dmitrudi@gmail.com>
2023-02-26 01:26:19 +0200

    вирішує 9ту задачу

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const resultEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  resultEl.textContent = getRandomHexColor();
+});

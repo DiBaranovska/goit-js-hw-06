7dade58d84debeee4023a4cfb7def4751de09e8a
Diana Baranovska <dmitrudi@gmail.com>
2023-02-26 01:08:56 +0200

    вирішує 8му задачу

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.forms[0];
 
+const mailEl = formEl.elements.email;
+const passwordEl = formEl.elements.password;
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  if (mailEl.value && passwordEl.value !== '') {
+    console.log({
+      name: mailEl.value,
+      password: passwordEl.value,
+    });
+    event.target.reset();
+  } else {
+    alert('Мають бути заповнені усі поля');
+  }
+});

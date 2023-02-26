a6842ba25015f153e1c8aac50fd7716186ec6da5
Diana Baranovska <dmitrudi@gmail.com>
2023-02-26 03:08:52 +0200

    рішення 5ї задачі

--- js/task-02.js
+++ js/task-02.js
@@ -1,17 +1,10 @@
-const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
-];
-let listEl = document.querySelector("#ingredients");
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+let listEl = document.querySelector('#ingredients');
 
-const textItem = ingredients.map((element) => {
-  const itemEl = document.createElement("li");
+const textItem = ingredients.map(element => {
+  const itemEl = document.createElement('li');
   itemEl.textContent = element;
-  itemEl.classList.add("item");
+  itemEl.classList.add('item');
   return itemEl;
 });
 listEl.append(...textItem);
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const conteinerEl = document.querySelector('#controls');
+const inputEl = conteinerEl.firstElementChild;
+const buttonCreateEl = inputEl.nextElementSibling;
+const buttonDestroyEl = conteinerEl.lastElementChild;
+const newConteinersEls = document.querySelector('#boxes');
+const amount = 0;
+let conteinerList = [];
+let height = 20;
+let width = 20;
+function createBoxes(amount) {
+  amount = inputEl.value;
+  for (let i = 0; i < amount; i += 1) {
+    const itemEl = document.createElement('div');
+    itemEl.style.height = `${(height += 10)}px`;
+    itemEl.style.width = `${(width += 10)}px`;
+    itemEl.style.backgroundColor = getRandomHexColor();
+    conteinerList.push(itemEl);
+  }
+  return newConteinersEls.append(...conteinerList);
+}
+buttonCreateEl.addEventListener('click', createBoxes);
+
+buttonDestroyEl.addEventListener('click', event => {
+  newConteinersEls.innerHTML = '';
+});
